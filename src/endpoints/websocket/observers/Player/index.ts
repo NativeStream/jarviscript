0860412f66a9321c141a6ca770ed4f1e8e1e6d3e
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-08-11 15:41:35 -0300

    refact: Added global events of player to be emmited to websocket

--- src/endpoints/websocket/observers/Player/index.ts
+++ src/endpoints/websocket/observers/Player/index.ts
@@ -1,3 +1,23 @@
-import PlayerQueryAdded from "./PlayerQueryAdded";
+import playerEvents from "../../../../interfaces/player/events";
+import app, { Observer, EventData } from "../../../../app";
+import events from "../../events";
 
-export default [PlayerQueryAdded];
+class PlayerObserver implements Observer {
+  event: string;
+  callback(eventData: EventData): void {
+    app.notify(events.SOCKET_SEND, eventData);
+  }
+  constructor(event: string) {
+    this.event = event;
+  }
+}
+
+function playerObserversCreate() {
+  let playerObservers: Array<Observer> = [];
+  for (const event in playerEvents) {
+    playerObservers.push(new PlayerObserver(event));
+  }
+  return playerObservers;
+}
+
+export default playerObserversCreate();
