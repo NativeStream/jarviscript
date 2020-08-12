7002da1a80b649c0c2634097197b633bac4aee47
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-08-12 09:45:44 -0300

    refact: Fixed player observers

--- src/endpoints/websocket/observers/Player/index.ts
+++ src/endpoints/websocket/observers/Player/index.ts
@@ -1,20 +1,26 @@
 import playerEvents from "../../../../interfaces/player/events";
 import app, { Observer, EventData } from "../../../../app";
 import events from "../../events";
+import LoggerBuilder from "../../../../logs/LoggerBuilder";
 
 class PlayerObserver implements Observer {
   event: string;
-  callback(eventData: EventData): void {
-    app.notify(events.SOCKET_SEND, eventData);
-  }
+
   constructor(event: string) {
     this.event = event;
+  }
+
+  callback(eventData: EventData): void {
+    app.notify(events.SOCKET_SEND, {
+      ...eventData,
+      event: this.event,
+    });
   }
 }
 
 function playerObserversCreate() {
   let playerObservers: Array<Observer> = [];
-  for (const event in playerEvents) {
+  for (const event in playerEvents.emit) {
     playerObservers.push(new PlayerObserver(event));
   }
   return playerObservers;
